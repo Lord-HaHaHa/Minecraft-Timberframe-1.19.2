d4fa22dfd96410d418662a0960d927df14176ddb
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-03-08 20:05:06 +0100

    fixed loot

--- src/main/java/de/lordhahaha/timberframemod/block/ModBlocks.java
+++ src/main/java/de/lordhahaha/timberframemod/block/ModBlocks.java
@@ -8,6 +8,7 @@ import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,29 +22,31 @@ public class ModBlocks {
     public static final DeferredRegister<Block> BLOCKS =
             DeferredRegister.create(ForgeRegistries.BLOCKS, Timberframemod.MOD_ID);
 
+    // Define Material with values
+    private static final BlockBehaviour.Properties TimberframeMaterial = BlockBehaviour.Properties.of(Material.WOOD).strength(2.0F, 3.0F).sound(SoundType.WOOD);
     // Register all new blocks
-    public static final RegistryObject<Block> BASIC_BLOCK = registerBlock("basic_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> CROSS_BLOCK = registerBlock("cross_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> DOUBLESSTRAIGHT_BLOCK = registerBlock("doublestraight_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> DOWN_BLOCK = registerBlock("down_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> DOWN_L_FRAME_BLOCK = registerBlock("downlframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> DOWN_R_FRAME_BLOCK = registerBlock("downrframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> EDGE_BLOCK = registerBlock("edge_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> HEXAFRAME_BLOCK = registerBlock("hexaframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> MAN_LEFT_BLOCK = registerBlock("manleft_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> MAN_RIGHT_BLOCK = registerBlock("manright_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> PEDIMENT_LEFT_BLOCK = registerBlock("pedimentleft_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> PEDIMENT_RIGHT_BLOCK = registerBlock("pedimentright_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> PLASTER_BLOCK = registerBlock("plaster_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> RING_CROSS_FRAME_BLOCK = registerBlock("ringcrossframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> SHUTTER_LEFT_BLOCK = registerBlock("shutterleft_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> SHUTTER_RIGHT_BLOCK = registerBlock("shutterright_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> SINGLE_STRAIGHT_BLOCK = registerBlock("singlestraight_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> TOPEDGE_BLOCK = registerBlock("topedge_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> TOPFRAME_BLOCK = registerBlock("topframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> UP_BLOCK = registerBlock("up_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> UP_LEFT_FRAME_BLOCK = registerBlock("uplframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> UP_RIGHT_FRAME_BLOCK = registerBlock("uprframe_block", () -> new RotationalBlock(BlockBehaviour.Properties.of(Material.WOOD)) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> BASIC_BLOCK = registerBlock("basic_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> CROSS_BLOCK = registerBlock("cross_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> DOUBLESTRAIGHT_BLOCK = registerBlock("doublestraight_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> DOWN_BLOCK = registerBlock("down_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> DOWN_L_FRAME_BLOCK = registerBlock("downlframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> DOWN_R_FRAME_BLOCK = registerBlock("downrframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> EDGE_BLOCK = registerBlock("edge_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> HEXAFRAME_BLOCK = registerBlock("hexaframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> MAN_LEFT_BLOCK = registerBlock("manleft_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> MAN_RIGHT_BLOCK = registerBlock("manright_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> PEDIMENT_LEFT_BLOCK = registerBlock("pedimentleft_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> PEDIMENT_RIGHT_BLOCK = registerBlock("pedimentright_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> PLASTER_BLOCK = registerBlock("plaster_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> RING_CROSS_FRAME_BLOCK = registerBlock("ringcrossframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> SHUTTER_LEFT_BLOCK = registerBlock("shutterleft_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> SHUTTER_RIGHT_BLOCK = registerBlock("shutterright_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> SINGLE_STRAIGHT_BLOCK = registerBlock("singlestraight_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> TOPEDGE_BLOCK = registerBlock("topedge_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> TOPFRAME_BLOCK = registerBlock("topframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> UP_BLOCK = registerBlock("up_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> UP_LEFT_FRAME_BLOCK = registerBlock("uplframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> UP_RIGHT_FRAME_BLOCK = registerBlock("uprframe_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
 
 
     // --- Helper-functions ---
