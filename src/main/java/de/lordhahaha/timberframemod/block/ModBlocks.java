962b3982a1ab54f46d3abd17edd4ca653a4e4974
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-03-10 15:44:28 +0100

    Renamed "man" blocks

--- src/main/java/de/lordhahaha/timberframemod/block/ModBlocks.java
+++ src/main/java/de/lordhahaha/timberframemod/block/ModBlocks.java
@@ -45,8 +45,8 @@ public class ModBlocks {
     public static final RegistryObject<Block> HEXA_BLOCK = registerBlock("hexa_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
     public static final RegistryObject<Block> MAN_LEFT_BLOCK = registerBlock("man_left_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
     public static final RegistryObject<Block> MAN_RIGHT_BLOCK = registerBlock("man_right_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> PEDIMENT_LEFT_BLOCK = registerBlock("pedimentleft_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
-    public static final RegistryObject<Block> PEDIMENT_RIGHT_BLOCK = registerBlock("pedimentright_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> PEDIMENT_LEFT_BLOCK = registerBlock("pediment_left_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
+    public static final RegistryObject<Block> PEDIMENT_RIGHT_BLOCK = registerBlock("pediment_right_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
     public static final RegistryObject<Block> SHUTTER_LEFT_BLOCK = registerBlock("shutterleft_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
     public static final RegistryObject<Block> SHUTTER_RIGHT_BLOCK = registerBlock("shutterright_block", () -> new RotationalBlock(TimberframeMaterial) {}, ModCreativeModeTab.TIMBERFRAME_TAB);
 
