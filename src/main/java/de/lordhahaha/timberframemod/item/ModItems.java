5386aedfcd3e5dcff4e3a4272ad330df23ab230d
Lord-HaHaHa <57814183+Lord-HaHaHa@users.noreply.github.com>
2023-03-11 16:41:35 +0100

    added frame item

--- src/main/java/de/lordhahaha/timberframemod/item/ModItems.java
+++ src/main/java/de/lordhahaha/timberframemod/item/ModItems.java
@@ -1,6 +1,7 @@
 package de.lordhahaha.timberframemod.item;
 
 import de.lordhahaha.timberframemod.Timberframemod;
+import de.lordhahaha.timberframemod.tab.ModCreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -13,8 +14,10 @@ public class ModItems {
 
     // Register all items of the Mod
     public static final RegistryObject<Item> WATTLE_DAUB = ITEMS.register("wattle_daub", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIMBERFRAME_TAB)));
-    public static final RegistryObject<Item> FRAME = ITEMS.register("frame", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIMBERFRAME_TAB)));
 
+    // Frames
+    public static final RegistryObject<Item> FRAME = ITEMS.register("frame", () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TIMBERFRAME_TAB)));
+    
     public static void register(IEventBus eventBus){
         ITEMS.register(eventBus);
     }
